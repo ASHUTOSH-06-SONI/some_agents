9ba25064ad12980d696d489d7b128021b55d248d
Ashutosh Soni <91798960+ASHUTOSH-06-SONI@users.noreply.github.com>
2025-12-07 20:56:26 +0530

    Add API functions for request and logistics handling

---
+++ frontend/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000/api';
+
+export const submitRequest = async (data) => {
+    const response = await axios.post(`${API_URL}/request`, data);
+    return response.data;
+};
+
+export const checkStatus = async (requestId) => {
+    const response = await axios.get(`${API_URL}/request/${requestId}`);
+    return response.data;
+};
+
+export const updateLogistics = async (data) => {
+    const response = await axios.post(`${API_URL}/delivery/update`, data);
+    return response.data;
+};
+
+export const updateRepair = async (data) => {
+    const response = await axios.post(`${API_URL}/repair/update`, data);
+    return response.data;
+};
